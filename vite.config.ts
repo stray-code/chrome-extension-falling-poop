3a85b43fa81a2c4845090c54f3221c2aee9e0846
stray-code <abc1007020@gmail.com>
2024-04-11 23:26:24 +0900

    画像のディレクトリ移動

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,12 @@ const manifest = defineManifest({
   name: 'うんち落下',
   version: '1.0.0',
   icons: {
-    '16': 'assets/img/icon16.png',
-    '48': 'assets/img/icon48.png',
-    '128': 'assets/img/icon128.png',
+    '16': 'img/icon16.png',
+    '48': 'img/icon48.png',
+    '128': 'img/icon128.png',
   },
   action: {
-    default_icon: 'assets/img/icon16.png',
+    default_icon: 'img/icon16.png',
   },
   content_scripts: [
     {
