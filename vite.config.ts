00ef3a3f0f51ab5c6bf0f17126e31d90386ef3de
stray-code <abc1007020@gmail.com>
2024-04-11 23:26:05 +0900

    manifestをvite.config.tsに移動

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
+import { defineConfig } from 'vite';
+import { crx, defineManifest } from '@crxjs/vite-plugin';
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: 'うんち落下',
+  version: '1.0.0',
+  icons: {
+    '16': 'assets/img/icon16.png',
+    '48': 'assets/img/icon48.png',
+    '128': 'assets/img/icon128.png',
+  },
+  action: {
+    default_icon: 'assets/img/icon16.png',
+  },
+  content_scripts: [
+    {
+      js: ['src/content/main.ts'],
+      matches: ['http://*/*', 'https://*/*'],
+    },
+  ],
+  background: {
+    service_worker: 'src/background.ts',
+    type: 'module',
+  },
+  permissions: ['contextMenus'],
+});
 
 export default defineConfig({
   plugins: [crx({ manifest })],
-})
+});
